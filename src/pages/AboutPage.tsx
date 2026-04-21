6f3fbaa78f2e753b553c95607273cbc94686a2d6
SalatEh <234914038+salateh@users.noreply.github.com>
2026-04-21 12:30:45 +0300

    feat: add routing and navigation

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function AboutPage() {
+  return (
+    <div>
+      <p>Страница о проекте</p>
+      <img src="https://media.tenor.com/VdHmtmE4GE8AAAAe/absolute-cinema-kojima.png" />
+    </div>
+  );
+}
---
+++ src/pages/TasksPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function TasksPage() {
+  return (
+    <div>
+      <p>Страница задач</p>
+      <img src="https://media.tenor.com/VdHmtmE4GE8AAAAe/absolute-cinema-kojima.png" />
+    </div>
+  );
+}
